56d3cfdb2584919253521846f70ab9c55109b37e
Andrés Álvarez <andresjales@gmail.com>
2022-03-23 07:42:43 -0500

    Matrix exercise

---
+++ Assets/Scripts/MatrixExperiments.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatrixExperiments : MonoBehaviour
+{
+    [SerializeField] private Transform someObject;
+
+    [SerializeField] private float rotationZ = 0;
+    [SerializeField] private Vector3 scale = Vector3.one;
+    [SerializeField] private Vector3 pos = Vector3.right;
+
+    private void Update()
+    {
+        someObject.position = new Vector3(4, 4, 0);
+        var myMatrix = Matrix4x4.TRS(pos, Quaternion.Euler(0f, 0f, rotationZ), scale);
+        var newPos = myMatrix.MultiplyVector(someObject.position);
+        someObject.position = newPos;
+    }
+}
